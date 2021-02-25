599dc0117dd7964767b89656a22e8bd3140bff34
DarkoiV <makkob997@gmail.com>
2021-02-25 19:24:21 +0100

    Removed action wait, since all units should be able to wait

--- src/globals.hpp
+++ src/globals.hpp
@@ -32,7 +32,6 @@ void quit();
 constexpr Uint8 ACTION_ATTACK = 1;
 constexpr Uint8 ACTION_MAGIC = 2;
 constexpr Uint8 ACTION_HEAL = 4;
-constexpr Uint8 ACTION_WAIT = 8;
 
 //Structures/enums definitions
 struct vec2D{
