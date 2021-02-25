fc7c68e6c1c62f17beb77088898e07d7b0344094
DarkoiV <makkob997@gmail.com>
2021-02-25 19:31:54 +0100

    Removed action wait from unit, since it's no longer used

--- src/unit.cpp
+++ src/unit.cpp
@@ -15,13 +15,13 @@ cUnit::cUnit(std::string p_sUnitType){
 		m_pSprite = assets.getSprite("infantry");
 		m_pRangeTile = assets.getSprite("rangeTile");
 		m_unitAttributes.mov = 5;
-		m_unitPossibleActionFlags = ACTION_ATTACK + ACTION_WAIT;
+		m_unitPossibleActionFlags = ACTION_ATTACK;
 	}
 	else if(p_sUnitType == "enemyInfantry"){
 		m_pSprite = assets.getSprite("enemyInfantry");
 		m_pRangeTile = assets.getSprite("rangeTile");
 		m_unitAttributes.mov = 5;
-		m_unitPossibleActionFlags = ACTION_ATTACK + ACTION_WAIT;
+		m_unitPossibleActionFlags = ACTION_ATTACK;
 	}
 	else{
 		std::cout << "[ERROR] Wrong unit type" << std::endl;
