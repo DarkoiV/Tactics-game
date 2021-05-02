ccd30b8a16eb8cf0fe4aa1ec4e395020220e663f
DarkoiV <makkob997@gmail.com>
2021-05-02 14:16:46 +0200

    Added global enums

---
+++ src/enums.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+//Enum of possible scenes in game
+enum class eSCENE {
+	MAIN_MENU,
+	SETTINGS,
+	BATTLE,
+	EDIT
+};
--- src/game.hpp
+++ src/game.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
+#include "scene.hpp"
+#include "enums.hpp"
+#include <vector>
+
 class cGame{
 	private:
-		bool m_running = false;
+		// Game state
+		bool   m_running      = false         ;
+		eSCENE m_currentScene = eSCENE::BATTLE;
+
+		// Container of scenes pointers
+		std::vector<cScene*> scenesVector;
 
 	public:
 		// Init game
