19456e04ec37389385a39321554d0a17d54ef8ef
DarkoiV <makkob997@gmail.com>
2021-05-09 16:48:37 +0200

    Fixed turn interface

--- src/turn.hpp
+++ src/turn.hpp
@@ -7,14 +7,14 @@
 class cTurn{
 	public:
 		// Destructor
-		virtual ~cTurn();
+		virtual ~cTurn(){};
 
 		// Methods
-		virtual void start();
-		virtual bool isCompleted();
+		virtual void start()       = 0;
+		virtual bool isCompleted() = 0;
 
 		// Turn Loop
-		virtual void process(eBUTTON p_input);
-		virtual void update();
-		virtual void draw(vec2D p_cameraOffsety);
+		virtual void process(eBUTTON p_input)    = 0;
+		virtual void update()                    = 0;
+		virtual void draw(vec2D p_cameraOffsety) = 0;
 };
