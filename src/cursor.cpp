fadbf970cf6fed970101773eca5dfe49bfb5fc6b
DarkoiV <makkob997@gmail.com>
2021-05-09 14:01:19 +0200

    Added cursor class

---
+++ src/cursor.cpp
@@ -0,0 +1,49 @@
+#include "cursor.hpp"
+#include "asset_manager.hpp"
+
+// Initialize cursor on board
+void cCursor::init(vec2D p_boardSize){
+	// Set board size
+	m_boardSize = p_boardSize;
+
+	// Load sprite
+	auto &assetAccess = cAssetManager::getInstance();
+	m_sprite = assetAccess.getSprite("Cursor", "cursor");
+}
+
+// Move cursor up
+void cCursor::moveUp(){
+	if(m_position.y > 0)
+		m_position.y--;
+	else
+		std::cout << "[INFO] Cursor at board limit" << std::endl;
+}
+
+// Move cursor down
+void cCursor::moveDown(){
+	if(m_position.y < m_boardSize.y)
+		m_position.y++;
+	else
+		std::cout << "[INFO] Cursor at board limit" << std::endl;
+}
+
+// Move cursor left
+void cCursor::moveLeft(){
+	if(m_position.x > 0)
+		m_position.x--;
+	else
+		std::cout << "[INFO] Cursor at board limit" << std::endl;
+}
+
+// Move cursor right
+void cCursor::moveRight(){
+	if(m_position.x < m_boardSize.x)
+		m_position.x++;
+	else
+		std::cout << "[INFO] Cursor at board limit" << std::endl;
+}
+
+// Get cursor position
+auto cCursor::position() -> vec2D{
+	return m_position;
+}
---
+++ src/cursor.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "struct_vec2D.hpp"
+#include <SDL2/SDL.h>
+
+class cCursor{
+	private:
+		// Position in TILES
+		vec2D m_position{0, 0};
+
+		// Size of board in TILES
+		vec2D m_boardSize{0, 0};
+
+		// Sprite used for drawing
+		SDL_Texture* m_sprite = nullptr;
+	public:
+		// Init on board
+		void init(vec2D p_boardSize);
+
+		// Move cursor
+		void moveUp();
+		void moveDown();
+		void moveLeft();
+		void moveRight();
+
+		// Get position
+		auto position() -> vec2D;
+};
