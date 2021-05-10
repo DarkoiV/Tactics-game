7573d4e368e3e92768de1dc2f540fb2a278e73bd
DarkoiV <makkob997@gmail.com>
2021-05-10 17:39:26 +0200

    Added unit class

---
+++ src/unit.cpp
@@ -0,0 +1,43 @@
+#include "unit.hpp"
+#include "globals.hpp"
+#include "asset_manager.hpp"
+
+// Test constructor
+cUnit::cUnit(){
+	auto &assetAcces = cAssetManager::getInstance();
+	m_sprite = assetAcces.getSprite("Test unit", "unit_infantry");
+}
+
+// Set position on board
+void cUnit::setPosition(vec2D p_pos){
+	m_pos = p_pos;
+}
+
+// Offset position for given vector
+void cUnit::offsetPosition(vec2D p_offset){
+	m_offset.x += p_offset.x;
+	m_offset.y += p_offset.y;
+}
+
+// Reset offset
+void cUnit::resetOffset(){
+	m_offset = {0, 0};
+}
+
+// Draw unit
+void cUnit::draw(vec2D p_cameraOffset, int p_animationFrame){
+	// Initialize drawing rectangles
+	SDL_Rect srcRect{ 0 + TILE_SIZE * (p_animationFrame / 15)
+			, 0
+			, TILE_SIZE
+			, TILE_SIZE
+			};
+
+	SDL_Rect dstRect{ m_pos.x * TILE_SIZE + m_offset.x + p_cameraOffset.x
+			, m_pos.y * TILE_SIZE + m_offset.y + p_cameraOffset.y + RENDER_OFFSET
+			, TILE_SIZE
+			, TILE_SIZE
+			};
+
+	SDL_RenderCopy(g_renderer, m_sprite, &srcRect, &dstRect);
+}
---
+++ src/unit.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "struct_vec2D.hpp"
+#include "struct_stats.hpp"
+#include <SDL2/SDL.h>
+
+class cUnit{
+	private:
+		// Position on map in tiles, offset in pixels
+		vec2D m_pos{-1, -1};
+		vec2D m_offset{0, 0};
+		static constexpr int RENDER_OFFSET = -3;
+
+		// Stats
+		sStats m_stats;
+
+		// Sprite
+		SDL_Texture* m_sprite;
+	public:
+		// Constructor
+		cUnit();
+
+		// Methods for position
+		void setPosition(vec2D p_pos);
+		void offsetPosition(vec2D p_offset);
+		void resetOffset();
+
+		// Drawing
+		void draw(vec2D p_cameraOffset, int p_animationFrame);
+};
