5a4dbb355fc8822077615371590f69474c566251
DarkoiV <makkob997@gmail.com>
2021-05-28 15:24:32 +0200

    Added == overload to vec2D

--- src/struct_vec2D.hpp
+++ src/struct_vec2D.hpp
@@ -10,3 +10,7 @@ struct vec2D{
 inline std::ostream& operator << (std::ostream &os, const vec2D &s){
 	return (os << s.x << "x" << s.y);
 }
+
+inline bool operator == (const vec2D &r, const vec2D &l){
+	return (r.x == l.x and r.y == l.y);
+}
