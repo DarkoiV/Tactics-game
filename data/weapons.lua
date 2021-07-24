f2b62e3be673121c9ef46e32fb5452a99f8558f6
DarkoiV <makkob997@gmail.com>
2021-07-24 21:16:45 +0200

    Added weapons stub

--- data/weapons.lua
+++ data/weapons.lua
@@ -1,8 +1,44 @@
-IRON_SWORD = {
-	name  = "Iron Sword",
-	iType = 2,
-	power = 1,
+-- DEFAULT ATTACK ------------------------------------
+
+DEFAULT_ATTACK = function()
+
+end
+
+-- IRON SPEAR ----------------------------------------
+
+IRON_SPEAR = {
+	name  = "Iron Spear",
+	iType = 1,		-- Item Type 0000 0001
+	power = 2,
 	actions = {
 		"Attack"
 	}
 }
+
+IRON_SPEAR["Attack"] = DEFAULT_ATTACK
+
+-- IRON SWORD ----------------------------------------
+
+IRON_SWORD = {
+	name  = "Iron Sword",
+	iType = 2,		-- Item Type 0000 0010
+	power = 2,
+	actions = {
+		"Attack"
+	}
+}
+
+IRON_SWORD["Attack"] = DEFAULT_ATTACK
+
+-- IRON AXE ------------------------------------------
+
+IRON_AXE = {
+	name  = "Iron Axe",
+	iType = 4,		-- Item Type 0000 0100
+	power = 2,
+	actions = {
+		"Attack"
+	}
+}
+
+IRON_AXE["Attack"] = DEFAULT_ATTACK
