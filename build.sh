c9867bb7cf8b97dbeb26a50681f81c04bbbe1181
DarkoiV <makkob997@gmail.com>
2021-08-04 17:37:13 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "--------------"
+echo "Starting build"
+echo "--------------"
+echo
+
+# check for ~ LUA ~
+if test -f "./lib/Lua5.4/src/lua.hpp"; then
+	echo "LUA is present"
+else
+	echo "LUA is not present, downloading"
+	curl https://www.lua.org/ftp/lua-5.4.3.tar.gz -o lua-5.4.3.tar.gz
+	echo
+
+	echo "Unpacking LUA"
+	tar -xzf lua-5.4.3.tar.gz
+	mv lua-5.4.3/src/* lib/Lua5.4
+	echo
+
+	echo "Deleting reaming files from LUA download"
+	rm -rf lua-5.4.3*
+	echo
+fi
+echo
+
+# check for build directory
+if ! test -d "./build"; then
+	mkdir build
+fi
+
+cd build
+
+cmake ..
+echo 
+
+make
