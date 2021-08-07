c20a5069e1edbec0cb61b51b96c5368722c212db
DarkoiV <makkob997@gmail.com>
2021-08-07 04:05:04 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-echo "--------------"
-echo "Starting build"
-echo "--------------"
 echo
+echo "------------------------------"
+echo "        Starting build"
+echo "------------------------------"
+echo
+
+# check for cmake
+if ! command -v cmake; then
+	echo "CMake is required for this build!"
+	exit 1
+fi
+echo "CMake was found"
+echo
+	
 
 # check for ~ LUA ~
 if test -f "./lib/Lua5.4/lua.hpp"; then
 	echo "LUA is present"
 else
+	# Check for curl in PATH
+	if ! command -v curl; then
+		echo "Lua is not present" 
+		echo "For auto download curl is required!"
+		exit 1
+	fi
+
+	# Check for tar in PATH
+	if ! command -v tar; then
+		echo "Lua is not present" 
+		echo "For auto download tar is required!"
+		exit 1
+	fi
+
 	echo "LUA is not present, downloading"
 	curl https://www.lua.org/ftp/lua-5.4.3.tar.gz -o lua-5.4.3.tar.gz
 	echo
@@ -18,9 +42,8 @@ else
 	mv lua-5.4.3/src/* lib/Lua5.4
 	echo
 
-	echo "Deleting reaming files from LUA download"
+	echo "Deleting reaming files after LUA download"
 	rm -r lua-5.4.3*
-	echo
 fi
 echo
 
