379c7967c460b39c800c07d26c7a2213b777a8e6
DarkoiV <makkob997@gmail.com>
2021-09-22 09:05:53 +0200

    Changes to action menu

--- include/ui_action_menu.hpp
+++ include/ui_action_menu.hpp
@@ -3,29 +3,50 @@
 #include "unit.hpp"
 #include "battle_lua.hpp"
 #include "enums.hpp"
+#include "ui_box.hpp"
+#include "ui_text.hpp"
 
 class cActionMenu {
 	private:
+		// Reference to lua state
 		cBattleLua &Lua;
+
+		// Pointer to unit for whom aMenu is drawn, or nullptr for hiden
+		cUnit* unit;
+	
+		// Background box, and texts displaying subMenu and options
+		cBox  m_box;
+		cText m_subMenuTitle;
+		std::vector<cText> m_textOptions;
+
+		// Variables
+		bool   m_subMenuVisible	= false;			// Is subMenuTitle visible?
+		bool   m_visible 	= false;			// Is menu visible?
+		int    m_highlighted	= 0;				// Which option is highlighted
 
 	public:
 		cActionMenu(cBattleLua &L):
-			Lua(L){};
+			Lua(L),
+			m_subMenuTitle({0,0}, eTEXT_COLOR::YELLOW){};
 
+		// Move selection inside action menu
 		void moveUp();
 		void moveDown();
-		void moveRight();
-		void moveLeft();
 
+		// Select or cancel
 		void select();
 		void cancel();
 
+		// Display for unit, or nullptr to hide
 		void operator[](cUnit* p_unit);
 
+		// Return whether action was selected
 		bool isSelected();
 		auto getSelectedAction() -> eACTION;
 
+		// Autoresize action menu
+		void autoresize();
 
-
+		// Draw action menu
 		void draw();
 };
--- src/ui_action_menu.cpp
+++ src/ui_action_menu.cpp
@@ -1,23 +1,23 @@
 #include "ui_action_menu.hpp"
 
-void cActionMenu::draw() {
-
-}
-
 void cActionMenu::moveUp() {
-
+	if(m_highlighted == 0)
+		std::cout << "[INFO] Selection at top" << std::endl;
+	else {
+		m_textOptions[m_highlighted].changeTextColor(eTEXT_COLOR::YELLOW);
+		m_highlighted--;
+		m_textOptions[m_highlighted].changeTextColor(eTEXT_COLOR::RED);
+	}
 }
 
 void cActionMenu::moveDown() {
-
-}
-
-void cActionMenu::moveRight() {
-
-}
-
-void cActionMenu::moveLeft() {
-
+	if(m_highlighted == (int)m_textOptions.size() - 1)
+		std::cout << "[INFO] Selection at bottom" << std::endl;
+	else {
+		m_textOptions[m_highlighted].changeTextColor(eTEXT_COLOR::YELLOW);
+		m_highlighted++;
+		m_textOptions[m_highlighted].changeTextColor(eTEXT_COLOR::RED);
+	}
 }
 
 void cActionMenu::select() {
@@ -29,7 +29,17 @@ void cActionMenu::cancel() {
 }
 
 void cActionMenu::operator[](cUnit* p_unit) {
+	// Hide for nullptr
+	if(p_unit == nullptr){
+		m_visible = false;
+		return;
+	}	
+	else m_visible = true;
 
+	// Check if menu was constructed for unit, if so return
+	if(p_unit == unit) return;
+
+	// If not construct general page for unit
 }
 
 bool cActionMenu::isSelected() {
@@ -38,4 +48,51 @@ bool cActionMenu::isSelected() {
 
 auto cActionMenu::getSelectedAction() -> eACTION {
 	return eACTION::WAIT;
+}
+
+void cActionMenu::autoresize() {
+	// Get widest text
+	int widestText = 0;
+	for(size_t i = 0; i < m_textOptions.size(); i++) {
+		int width = m_textOptions[i].getPixelWidth();
+
+		if(width > widestText) widestText = width;
+	}
+	if(m_subMenuVisible and m_subMenuTitle.getPixelWidth() > widestText)
+		widestText = m_subMenuTitle.getPixelWidth();
+
+	// Set box
+	int textHeight = m_textOptions.back().getPixelHeight();
+	int boxInnerHeight = (textHeight + 2) * m_textOptions.size();
+	if(m_subMenuVisible) boxInnerHeight += textHeight * 2;
+
+	m_box.setCapacity({widestText, boxInnerHeight}, 1);
+	m_box.setOrigin({0, 0});
+
+	// Set text orgins
+	vec2D textOrigin = m_box.getInnerOrigin();
+	if(m_subMenuVisible) textOrigin.y += textHeight * 2;
+	for(size_t i = 0; i < m_textOptions.size(); i++) {
+		m_textOptions[i].setOriginPoint(textOrigin);
+		textOrigin.y += textHeight + 2;
+	}
+
+	// Set submenu title
+	if(m_subMenuVisible) {
+		vec2D subMenuOrigin = m_box.getInnerOrigin();
+		subMenuOrigin.x = subMenuOrigin.x + (widestText/2 - m_subMenuTitle.getPixelWidth()/2);
+		m_subMenuTitle.setOriginPoint(subMenuOrigin);
+	}
+}
+
+
+void cActionMenu::draw() {
+	if(m_visible) {
+		m_box.draw();
+		for(auto &text : m_textOptions){
+			text.draw();
+		}
+
+		if(m_subMenuVisible) m_subMenuTitle.draw();
+	}
 }
