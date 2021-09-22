f6f60ea579fe5586f171b0e34c1b4b6d8fc55fb2
DarkoiV <makkob997@gmail.com>
2021-09-22 09:06:02 +0200

    New inventory methods

--- include/unit_inventory.hpp
+++ include/unit_inventory.hpp
@@ -16,5 +16,12 @@ class cUnitInventory{
 		cUnitInventory(cUnit &u):
 			unit(u){};
 
+		// Get first item in inventory, or nullptr if none
 		auto getFirstItem() -> cItem*;
+
+		// Adds item to inventory
+		bool addNewItem(cItem p_item);
+
+		// Moves item at pos to first place
+		void makeFirst(int p_pos);
 };
--- src/unit_inventory.cpp
+++ src/unit_inventory.cpp
@@ -1,5 +1,17 @@
 #include "unit_inventory.hpp"
 
 auto cUnitInventory::getFirstItem() -> cItem* {
-	return nullptr;
+	if(m_items.size() == 0) return nullptr;	
+	return &m_items[0];
+}
+
+bool cUnitInventory::addNewItem(cItem p_item) {
+	if(m_items.size() >= 5) return false;
+	m_items.push_back(std::move(p_item));
+	return true;
+}
+
+void cUnitInventory::makeFirst(int p_pos) {
+	if(m_items.size() > p_pos or p_pos <= 0) return;
+	std::swap(m_items[0], m_items[p_pos]);
 }
