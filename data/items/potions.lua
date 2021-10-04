31b3f5034e5c4ffa8c9c21d0e34f8319f6aecbae
DarkoiV <makkob997@gmail.com>
2021-10-04 21:10:20 +0200

    Added health potion data

---
+++ data/items/potions.lua
@@ -0,0 +1,10 @@
+require("data/items/sequences.lua")
+
+-- HEALTH POTION -------------------------------------
+
+HEALTH_POTION = {
+	name   = "Health Potion",
+	iType  = ITEM_POTION,
+	power  = 5,
+	action = "Use"
+}
