4f67ca16854eee995e57f257793a0da67455f114
DarkoiV <makkob997@gmail.com>
2021-10-07 18:58:19 +0200

    Added max hp nad max mp stats

--- include/struct_stats.hpp
+++ include/struct_stats.hpp
@@ -6,6 +6,8 @@
 struct sStats{
 	uint8_t HP;		// Health Points
 	uint8_t MP;		// Mana Points
+	uint8_t MHP;		// Max HP
+	uint8_t MMP;		// Max MP
 	uint8_t DEF;		// Defence     (for physical Defence)
 	uint8_t STR;		// Strenght    (for physical DMG)
 	uint8_t AGI;		// Agility     (for accuracy, and dodge)
